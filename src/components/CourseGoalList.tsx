4f2d7e9724ddeaec7a03e0ff16e34498ff631b19
sagarmj94 <sagarmj1502@gmail.com>
2026-02-21 10:26:08 +0530

    feat(infobox): use discriminated union to enforce mode-specific props
    - Add HintBoxProps and WarningBoxProps
    - Enforce severity only for warning mode
    - Improve type safety with control flow narrowing

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
 import CourseGoal from "./CourseGoal";
 import { type CourseGoal as CGoal } from "../App.tsx";
+import InfoBox from "./infoBox.tsx";
+import type { ReactNode } from "react";
 
 type CourseGoalList = {
   goals: CGoal[];
@@ -10,9 +12,26 @@ export default function CourseGoalList({
   goals,
   onDeleteGoal,
 }: CourseGoalList) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">You have not goal yet. Start adding some</InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  console.log("goals", goals);
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning" severity="medium">
+        You're collecting a lot of goals. Don't put too much on your plate
+      </InfoBox>
+    );
+  }
   return (
     <>
       <ul>
+        {warningBox}
         {goals.map(({ title, description, id }) => (
           <li key={id}>
             <CourseGoal id={id} title={title} onDelete={onDeleteGoal}>
